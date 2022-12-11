cbb727a625caafa45c4946b38b5c2dd66014fe6e
Vivian Trieu <vivian.dtrieu@gmail.com>
2022-12-11 15:58:43 -0800

    Format changes

--- src/FloydWarshalls.java
+++ src/FloydWarshalls.java
@@ -3,45 +3,36 @@ class FloydWarshalls
     private int distanceMatrix[][];
     private int numberOfVertices;
    
-    public FloydWarshalls(int numberOfVertices)
-    {
+    public FloydWarshalls(int numberOfVertices) {
         distanceMatrix = new int[numberOfVertices + 1][numberOfVertices + 1];
         this.numberOfVertices = numberOfVertices;
     }
  
-    public void floydwarshallWorking(int graph[][])
-    {
-        for (int i = 1; i <= numberOfVertices; i++)
-        {
-            for (int j = 1; j <= numberOfVertices; j++)
-            {
+    public void floydwarshallWorking(int graph[][]) {
+
+        for (int i = 1; i <= numberOfVertices; i++) {
+            for (int j = 1; j <= numberOfVertices; j++) {
                 distanceMatrix[i][j] = graph[i][j];
             }
         }
  
-        for (int intermediate = 1; intermediate <= numberOfVertices; intermediate++)
-        {
-            for (int i = 1; i <= numberOfVertices; i++)
-            {
-                for (int j = 1; j <= numberOfVertices; j++)
-                {
-                    if (distanceMatrix[i][intermediate] + distanceMatrix[intermediate][j]
-                         < distanceMatrix[i][j])
-                        distanceMatrix[i][j] = distanceMatrix[i][intermediate] 
-                            + distanceMatrix[intermediate][j];
+        for (int intermediate = 1; intermediate <= numberOfVertices; intermediate++) {
+            for (int i = 1; i <= numberOfVertices; i++) {
+                for (int j = 1; j <= numberOfVertices; j++) {
+                    if (distanceMatrix[i][intermediate] + distanceMatrix[intermediate][j] < distanceMatrix[i][j])
+                        distanceMatrix[i][j] = distanceMatrix[i][intermediate] + distanceMatrix[intermediate][j];
                 }
             }
         }
  
         for (int i = 1; i <= numberOfVertices; i++)
             System.out.print("\t" + i);
- 
+        
         System.out.println();
-        for (int i = 1; i <= numberOfVertices; i++)
-        {
+
+        for (int i = 1; i <= numberOfVertices; i++) {
             System.out.print(i + "\t");
-            for (int j = 1; j <= numberOfVertices; j++)
-            {
+            for (int j = 1; j <= numberOfVertices; j++){
                 System.out.print(distanceMatrix[i][j] + "\t");
             }
             System.out.println();
