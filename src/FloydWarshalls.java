1eea71b8535d55ebc3f14521902da321758ce1ba
Vivian Trieu <vivian.dtrieu@gmail.com>
2022-12-11 17:51:46 -0800

    Rewriting class

--- src/FloydWarshalls.java
+++ src/FloydWarshalls.java
@@ -1,43 +1,57 @@
 class FloydWarshalls
 {
-    private int distanceMatrix[][];
-    private int numberOfVertices;
-   
-    public FloydWarshalls(int numberOfVertices) {
-        distanceMatrix = new int[numberOfVertices + 1][numberOfVertices + 1];
-        this.numberOfVertices = numberOfVertices;
-    }
- 
-    public void floydwarshallWorking(int graph[][]) {
+    /*
+ * Floyd-Warshall Algorithm:
+ * n = no of vertices
+ * A = matrix of dimension n*n
+ * for i = 1 to n
+ *      for j = 1 to n
+ *          A0[i, j] = w[i, j]
+ * for k = 1 to n
+ *      for i = 1 to n
+ *          for j = 1 to n
+ *              Ak[i, j] = min (Ak-1[i, j], Ak-1[i, k] + Ak-1[k, j])
+ * return A
+ */
 
-        for (int i = 1; i <= numberOfVertices; i++) {
-            for (int j = 1; j <= numberOfVertices; j++) {
-                distanceMatrix[i][j] = graph[i][j];
+    public int[][] floydwarshall(int graph[][], int n) {
+
+        int distMatrix[][] = new int[n][n];
+        int u, v, k;
+
+        for (u = 0; u < n; u++) {
+            for (v = 0; v < n; v++) {
+                distMatrix[u][v] = graph[u][v];
             }
         }
- 
-        for (int intermediate = 1; intermediate <= numberOfVertices; intermediate++) {
-            for (int i = 1; i <= numberOfVertices; i++) {
-                for (int j = 1; j <= numberOfVertices; j++) {
-                    if (distanceMatrix[i][intermediate] + distanceMatrix[intermediate][j] < distanceMatrix[i][j])
-                        distanceMatrix[i][j] = distanceMatrix[i][intermediate] + distanceMatrix[intermediate][j];
+
+        for (k = 0; k < n; k++) {
+            for (u = 0; u < n; u++) {
+                for (v = 0; v < n; v++) {
+                    if (distMatrix[u][k] + distMatrix[k][v] < distMatrix[u][v]) {
+                        distMatrix[u][v] = distMatrix[u][k] + distMatrix[k][v];
+                    }
                 }
             }
         }
- 
-        for (int i = 1; i <= numberOfVertices; i++)
+        return distMatrix;
+    }
+    
+    void printMatrix(int matrix[][], int n) {
+        for (int i = 0; i < n; i++)
             System.out.print("\t" + i);
         
         System.out.println();
 
-        for (int i = 1; i <= numberOfVertices; i++) {
+        for (int i = 0; i < n; ++i) {
             System.out.print(i + "\t");
-            for (int j = 1; j <= numberOfVertices; j++){
-                System.out.print(distanceMatrix[i][j] + "\t");
+            for (int j = 0; j < n; ++j) {
+                System.out.print(matrix[i][j] + "\t");
             }
             System.out.println();
-        }
+          }
     }
- 
+    
 }
+ 
 
